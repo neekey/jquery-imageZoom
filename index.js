ebec314104afdb08b2ff1a2c808a1c30d2913bd5
neekey <ni184775761@gmail.com>
2012-04-29 13:53:27 +0800

    init

---
+++ index.js
@@ -0,0 +1,246 @@
+(function(){
+
+    $( document).ready(function (){
+
+        var imgZoom = new ImageZoom( '#img-to-zoom' );
+
+    });
+
+    var ImageZoom = function ( img ){
+
+        this.ifImgWrapInitialized = false;
+        this.img = $( img );
+        this.maxRadio = 1.5;
+        this.zoomOffset = 0.1;
+        this.maxWidth = undefined;
+        this.maxHeight = undefined;
+        this.originWidth = undefined;
+        this.originHeight = undefined;
+
+        this.imgWrapInit();
+        this.attach();
+    };
+
+    ImageZoom.prototype = {
+
+        imgWrapInit: function (){
+
+            if( !this.ifImgWrapInitialized ){
+
+                this.imgWrap = this.img.wrap('<div class="image-zoom-wrap"></div>').parent();
+                this.enlargeBtn = $( '<span class="image-zoom-enlarge"></span>').hide();
+                this.narrowBtn = $( '<span class="image-zoom-narrow"></span>').hide();
+                this.img.addClass( 'image-zoom-target' );
+
+                this.imgWrap.append( this.enlargeBtn).append( this.narrowBtn );
+
+                var imgWidth =  this.img.width();
+                var imgHeight = this.img.height();
+
+                if( parseInt( imgWidth ) > 0 && parseInt( imgHeight ) ){
+
+                    this.ifImgWrapInitialized = true;
+                    this.imgWrap.css({
+                        width: imgWidth,
+                        height: imgHeight
+                    });
+
+                    this.calImgSize();
+                }
+                else {
+
+                    var that = this;
+
+                    this.setLoading( true );
+
+                    this.img.on( 'load', function (){
+
+                        imgWidth = that.img.width();
+                        imgHeight = that.img.height();
+
+                        that.imgWrap.css({
+                            width: imgWidth,
+                            height: imgHeight
+                        });
+
+                        that.setLoading( false );
+                        that.calImgSize();
+                    });
+                }
+            }
+
+        },
+
+        /**
+         * 计算图像的原始值，最大颗放大值
+         */
+        calImgSize: function (){
+
+            this.maxWidth = parseInt( this.img.width() ) * this.maxRadio;
+            this.maxHeight = parseInt( this.img.height() ) * this.maxRadio;
+            this.originWidth = parseInt( this.img.width() );
+            this.originHeight = parseInt( this.img.width() ) ;
+        },
+
+        /**
+         * 设置loading状态，loading时会隐藏img
+         */
+        setLoading: function ( ifLoading ){
+
+            var visibility = ifLoading ? 'hidden' : 'visible';
+
+            // 取消loading效果 并显示img
+            if( ifLoading ){
+                this.imgWrap.add( 'loading' );
+            }
+            else {
+                this.imgWrap.removeClass( 'loading' );
+            }
+
+            this.img.css({
+                visibility: visibility
+            });
+        },
+
+        attach: function (){
+
+            var that = this;
+
+            this.img[ 0 ].onmousewheel = function( e ){
+
+                var dir = e.wheelDelta === 0 ? e.detail : e.wheelDelta;
+
+                if( dir === 0 || dir === undefined ){
+                    return;
+                }
+                else {
+                    if( dir > 0 ){
+
+                        that.enlarge();
+                    }
+                    else {
+                        that.narrow();
+                    }
+
+                }
+            };
+
+            this.img.on( 'mouseenter', function (){
+
+                that.setZoomBtnPos();
+                that.enlargeBtn.show();
+                that.narrowBtn.show();
+            });
+
+            this.img.on( 'mouseout', function (){
+
+                that.enlargeBtn.hide();
+                that.narrowBtn.hide();
+            });
+
+            this.enlargeBtn.on( 'mouseenter', function (){
+
+                that.enlargeBtn.show();
+                that.narrowBtn.show();
+            }).on( 'click', function (){
+
+                that.enlarge();
+            });
+
+            this.narrowBtn.on( 'mouseenter', function (){
+
+                that.enlargeBtn.show();
+                that.narrowBtn.show();
+            }).on( 'click', function (){
+
+                that.narrow();
+            });
+
+
+        },
+
+        setZoomBtnPos: function (){
+
+            var imgWidth = parseInt( this.img.width() );
+            var imgHeight = parseInt( this.img.height() );
+            var gap = 10;
+            var btnWidth = parseInt( this.enlargeBtn.width());
+            var btnHeight = parseInt( this.enlargeBtn.height() );
+
+            var top = parseInt ( imgHeight / 2 - btnHeight / 2 );
+            var enlargeLeft = ( imgWidth / 2 - ( btnWidth * 2 + gap ) / 2 );
+            var narrowLeft = enlargeLeft + btnWidth + gap;
+
+            this.enlargeBtn.css({
+                top: top,
+                left: enlargeLeft
+            });
+
+            this.narrowBtn.css({
+                top: top,
+                left: narrowLeft
+            });
+        },
+
+        /**
+         * 放大
+         */
+        enlarge: function (){
+
+            var currentWidth = parseInt( this.img.width() );
+            var currentRadio = currentWidth / this.originWidth;
+            var targetRadio = currentRadio + this.zoomOffset;
+            var targetWidth = undefined;
+            var targetHeight = undefined;
+            var that = this;
+
+            if( targetRadio > this.maxRadio ){
+
+                targetRadio = this.maxRadio;
+            }
+
+            targetWidth = this.originWidth * targetRadio;
+            targetHeight = this.originHeight * targetRadio;
+
+            // 先停止已经在运行的动画
+            this.img.stop();
+            // 放大
+            this.img.animate({
+                width: targetWidth + 'px',
+                height: targetHeight + 'px'
+            }, 100, function (){
+
+                that.setZoomBtnPos();
+            });
+        },
+
+        /**
+         * 缩小
+         */
+        narrow: function (){
+
+            var currentWidth = parseInt( this.img.width() );
+            var currentRadio = currentWidth / this.originWidth;
+            var targetRadio = currentRadio - this.zoomOffset;
+            var targetWidth = undefined;
+            var targetHeight = undefined;
+
+            if( targetRadio < 1 ){
+
+                targetRadio = 1;
+            }
+
+            targetWidth = this.originWidth * targetRadio;
+            targetHeight = this.originHeight * targetRadio;
+
+            // 先停止已经在运行的动画
+            this.img.stop();
+            // 放大
+            this.img.animate({
+                width: targetWidth + 'px',
+                height: targetHeight + 'px'
+            }, 100);
+        }
+    }
+
+})();
